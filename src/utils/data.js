a1e39674d09c7706961c63e53e3180c41bf6969f
Robert <solitary.homelab@gmail.com>
2024-03-03 02:08:47 -0600

    Setup work experience component and styles

--- src/utils/data.js
+++ src/utils/data.js
@@ -39,3 +39,42 @@ export const SKILLS = [
     ],
   },
 ];
+
+export const WORK_EXPERIENCE = [
+  {
+    title: 'PLACEHOLDER',
+    date: 'PLACEHOLDER',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui justo, luctus ac feugiat id, tincidunt at magna.',
+      'Phasellus a nunc sit amet purus blandit tincidunt auctor vestibulum urna. Aenean egestas euismod augue, dapibus lacinia odio blandit eget.',
+      'In posuere quam id nisl tincidunt egestas. Fusce a mi eu dui auctor malesuada in a ipsum. Nullam pharetra faucibus justo in euismod.',
+    ],
+  },
+  {
+    title: 'PLACEHOLDER',
+    date: 'PLACEHOLDER',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui justo, luctus ac feugiat id, tincidunt at magna.',
+      'Phasellus a nunc sit amet purus blandit tincidunt auctor vestibulum urna. Aenean egestas euismod augue, dapibus lacinia odio blandit eget.',
+      'In posuere quam id nisl tincidunt egestas. Fusce a mi eu dui auctor malesuada in a ipsum. Nullam pharetra faucibus justo in euismod.',
+    ],
+  },
+  {
+    title: 'PLACEHOLDER',
+    date: 'PLACEHOLDER',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui justo, luctus ac feugiat id, tincidunt at magna.',
+      'Phasellus a nunc sit amet purus blandit tincidunt auctor vestibulum urna. Aenean egestas euismod augue, dapibus lacinia odio blandit eget.',
+      'In posuere quam id nisl tincidunt egestas. Fusce a mi eu dui auctor malesuada in a ipsum. Nullam pharetra faucibus justo in euismod.',
+    ],
+  },
+  {
+    title: 'PLACEHOLDER',
+    date: 'PLACEHOLDER',
+    responsibilities: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui justo, luctus ac feugiat id, tincidunt at magna.',
+      'Phasellus a nunc sit amet purus blandit tincidunt auctor vestibulum urna. Aenean egestas euismod augue, dapibus lacinia odio blandit eget.',
+      'In posuere quam id nisl tincidunt egestas. Fusce a mi eu dui auctor malesuada in a ipsum. Nullam pharetra faucibus justo in euismod.',
+    ],
+  },
+];
