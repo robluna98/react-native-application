9edf29c58457ecb933d9247d0cc48666f059b3b1
Robert <solitary.homelab@gmail.com>
2024-03-03 03:03:36 -0600

    Setup footer component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero/Hero';
 import Skills from './components/Skills/Skills';
 import WorkExperience from './components/WorkExperience/WorkExperience';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <WorkExperience />
         <Contact />
       </div>
+      <Footer />
     </>
   );
 };
