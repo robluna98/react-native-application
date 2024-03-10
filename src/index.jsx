7f72976a178d04ef2eb8460de3ce426b5178cef4
Robert.Dev <solitary.homelab@gmail.com>
2024-03-09 23:47:59 -0600

    Setup Normalize.css into App.css

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './index.css';
 import App from './App';
-import 'normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
