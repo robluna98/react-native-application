b81b9019e0e809067907718e2b5492b19ad6c103
Robert.Dev <solitary.homelab@gmail.com>
2024-03-09 22:39:05 -0600

    Setup notifications in contact form and reset form after submit

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -1,9 +1,25 @@
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './ContactForm.css';
 
 const ContactForm = () => {
   const form = useRef();
+
+  const notify = (message, isError = false) => {
+    if (isError) {
+      toast.error(message, {
+        draggable: true,
+        theme: 'colored',
+      });
+    } else {
+      toast.success(message, {
+        draggable: true,
+        theme: 'colored',
+      });
+    }
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -19,16 +35,20 @@ const ContactForm = () => {
       )
       .then(
         () => {
-          console.log('SUCCESS!');
+          notify('Email sent successfully.');
+          form.current.reset();
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          console.log(error);
+          notify(`Failed to send email: ' ${error}`, true);
+          form.current.reset();
         },
       );
   };
 
   return (
     <div className="contact-form-content">
+      <ToastContainer position="top-right" autoClose={5000} />
       <form ref={form}>
         <div className="name-container">
           <input type="text" name="user_name" placeholder="Name" />
